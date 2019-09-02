082765ff008678bb2b96517149f2a8c94e76b321
G-JP01 HIL01 <G-JP01.HIL01@veoneer.com>
2019-09-02 09:56:41 +0900

    production environment

--- react/src/environment/environment.js
+++ react/src/environment/environment.js
@@ -5,6 +5,10 @@ const api_server =
 
 const front_end_server =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "http://jp01-clarity01:3000";
+    ? "https://localhost:3000"
+    : "https://jp01-clarity01:3000";
+
+const clientID =   process.env.NODE_ENV === "development"
+? "11a9c8da-70ec-4ba1-9e4a-5267cf7f67b2"
+: "8d328dfa-6f5f-442e-baf0-f7cc55bb9ba1";
 export { api_server, front_end_server };

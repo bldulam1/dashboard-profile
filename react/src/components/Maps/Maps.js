a53632145272c88b6d06268e26c2037be867dde7
Brendon Dulam <brendon.dulam@veoneer.com>
2019-10-07 14:59:47 +0900

    streetscape.gl

--- react/src/components/Maps/Maps.js
+++ react/src/components/Maps/Maps.js
@@ -1,354 +1,190 @@
-import React, { useContext, useReducer, useState } from "react";
-import Axios from "axios";
-import { api_server } from "../../environment/environment";
-import { makeStyles, withStyles, useTheme } from "@material-ui/styles";
+import React, { PureComponent } from "react";
+import Paper from "@material-ui/core/Paper";
+import { setXVIZConfig, getXVIZConfig } from "@xviz/parser";
 import {
-  Slider,
-  TextField,
-  Tabs,
-  Tab,
-  FormControlLabel,
-  Switch
-} from "@material-ui/core";
-import uuid from "uuid/v4";
-import { MainMapContext } from "../../context/MainMap.Context";
-import ReactMapboxGl, {
-  GeoJSONLayer,
-  Marker,
-  MapContext
-} from "react-mapbox-gl";
-import carMarker from "../../assets/map-marker.svg";
+  LogViewer,
+  PlaybackControl,
+  StreamSettingsPanel,
+  MeterWidget,
+  TrafficLightWidget,
+  TurnSignalWidget,
+  XVIZPanel,
+  VIEW_MODE
+} from "streetscape.gl";
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    width: "100%",
-    borderLeft: "1px white solid",
-    overflow: "auto"
-  },
-  row1: {
-    display: "flex",
-    flexDirection: "row",
-    height: "85%"
-  },
-  leftSideBar: {
-    padding: "1rem",
-    display: "flex",
-    flexDirection: "column",
-    width: "25%",
-    // background: "#FFFFFF",
-    borderRight: "1px white solid",
-    alignItems: "center",
-    color: theme.palette.primary.contrastText
-  },
+import { Form } from "@streetscape.gl/monochrome";
 
-  row2: {
-    display: "flex",
-    flexDirection: "row",
-    height: "15%",
-    background: "#313131"
-  },
-  dashboard: {
-    display: "flex",
-    flexDirection: "column",
-    width: "25%",
-    background: "#313131",
-    borderTop: "1px white solid",
-    borderRight: "1px white solid",
-    justifyContent: "center",
-    alignItems: "center",
-    color: theme.palette.primary.contrastText
-  },
-  scroll: {
-    display: "flex",
-    flexDirection: "column",
-    width: "75%",
-    background: "#313131",
-    borderTop: "1px white solid",
-    justifyContent: "center",
-    alignItems: "center",
-    color: theme.palette.primary.contrastText,
-    padding: theme.spacing(2)
-  },
-  slider: { color: theme.palette.secondary.main, width: "95%" },
-  form: {
-    width: "100%",
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  notchedOutline: {
-    borderColor: "white",
-    borderWidth: 1,
-    "&:hover": {
-      borderColor: "white",
-      borderWidth: 2
-    }
-  }
-}));
+import {
+  XVIZ_CONFIG,
+  APP_SETTINGS,
+  MAPBOX_TOKEN,
+  MAP_STYLE,
+  XVIZ_STYLE,
+  CAR
+} from "./constants";
 
-const EnhancedMarker = props => {
-  const { carLocation } = props;
+setXVIZConfig(XVIZ_CONFIG);
+
+const TIMEFORMAT_SCALE =
+  getXVIZConfig().TIMESTAMP_FORMAT === "seconds" ? 1000 : 1;
+
+const exampleLog = require("./log-from-file").default;
+
+export default params => {
   return (
-    <Marker coordinates={carLocation} anchor="bottom">
-      <img src={carMarker} style={{ height: 50 }} />
-    </Marker>
+    <Paper
+      style={{
+        margin: "1rem",
+        padding: "1rem",
+        width: "100%"
+      }}
+    >
+      <StreetscapeGL />
+    </Paper>
   );
 };
 
-function reducer(state, action) {
-  return action;
-}
-
-let previousID = null;
-
-export default props => {
-  const [state, dispatch] = useReducer(reducer, {
-    inputFile:
-      "V:/JP01/DataLake/Common_Write/Subaru_DC/20190626_141439_FCR_SBR_AD2_R0500D512_DC_JSOIMPREZA/cache/20190626_141439_FCR_SBR_AD2_R0500D512_DC_JSOIMPREZA_split_F1.kml",
-    coordinates: [[0, 0]],
-    center: [0, 0],
-    distance: 0,
-    carLocation: [0, 0],
-    markers: [{value: 0}, {value:5}]
-  });
-
-  const { inputFile, coordinates, markers } = state;
-
-  React.useEffect(() => {
-    Axios.get(`${api_server}/maps/parse-kml/1/kml=${inputFile}`).then(
-      results => {
-        if (results.data.center && results.data.coordinates) {
-          dispatch(results.data);
-          console.log(results.data);
-        }
-      }
-    );
-  }, [inputFile]);
-
-  const handleChange = name => event => {
-    // dispatch({ [name]: event.target.value });
+class StreetscapeGL extends PureComponent {
+  state = {
+    log: exampleLog,
+    settings: {
+      viewMode: "PERSPECTIVE",
+      showTooltip: false
+    }
   };
-  const accessToken =
-    "pk.eyJ1IjoiYmxkdWxhbTEiLCJhIjoiY2p5YncwdHV2MGNuaDNjcW1mYnpxcnF5MiJ9._XwVCfgZCvePQBCvb-BUVA";
 
-  const Map = ReactMapboxGl({ accessToken });
-  const classes = useStyles();
-  let mapHandler = null;
+  componentDidMount() {
+    this.state.log.on("error", console.error).connect();
+  }
 
-  return (
-    <div className={classes.paper}>
-      <MainMapContext.Provider value={{ state, dispatch }}>
-        <div className={classes.row1}>
-          <div className={classes.leftSideBar}>
-            <form
-              className={classes.form}
-              noValidate
-              autoComplete="off"
-              onSubmit={event => event.preventDefault()}
+  _onSettingsChange = changedSettings => {
+    this.setState({
+      settings: { ...this.state.settings, ...changedSettings }
+    });
+  };
+
+  render() {
+    const { log, settings } = this.state;
+    const hudHR = { margin: 0, opacity: 0.3 };
+
+    return (
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          overflow: "hidden"
+        }}
+      >
+        <div
+          style={{
+            boxSizing: "border-box",
+            width: "320px",
+            padding: "12px",
+            position: "relative",
+            zIndex: 1,
+            boxShadow: "0 0 8px rgba(0,0,0,0.3)",
+            height: "100%",
+            overflow: "auto"
+          }}
+        >
+          <XVIZPanel log={log} name="Metrics" />
+          <hr
+            style={{
+              margin: "24px -12px",
+              opacity: 0.3
+            }}
+          />
+          <XVIZPanel log={log} name="Camera" />
+          <hr
+            style={{
+              margin: "24px -12px",
+              opacity: 0.3
+            }}
+          />
+          <Form
+            data={APP_SETTINGS}
+            values={this.state.settings}
+            onChange={this._onSettingsChange}
+          />
+          <StreamSettingsPanel log={log} />
+        </div>
+        <div
+          style={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            height: "100%"
+          }}
+        >
+          <div
+            style={{
+              flexGrow: 1,
+              position: "relative",
+              maxHeight: "100%"
+            }}
+          >
+            <LogViewer
+              log={log}
+              mapboxApiAccessToken={MAPBOX_TOKEN}
+              mapStyle={MAP_STYLE}
+              car={CAR}
+              xvizStyles={XVIZ_STYLE}
+              showTooltip={settings.showTooltip}
+              viewMode={VIEW_MODE[settings.viewMode]}
+            />
+            <div
+              style={{
+                position: "absolute",
+                top: "12px",
+                right: "12px",
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: "0 0 8px rgba(0,0,0,0.3)"
+              }}
             >
-              <TextField
-                id="inputFile-flexible"
-                label="Input File"
-                multiline
-                rowsMax="2"
-                value={inputFile}
-                onChange={handleChange("fileName")}
-                margin="none"
-                fullWidth
-                variant="outlined"
-                InputProps={{
-                  classes: {
-                    notchedOutline: classes.notchedOutline
-                  }
-                }}
+              <TurnSignalWidget log={log} streamName="/vehicle/turn_signal" />
+              <hr style={hudHR} />
+              <TrafficLightWidget
+                log={log}
+                streamName="/vehicle/traffic_light"
               />
-            </form>
-            <Tabs
-              onChange={handleChange}
-              value={0}
-              indicatorColor="primary"
-              textColor="primary"
-              variant="fullWidth"
-              aria-label="streams charts tab"
-            >
-              <Tab label="STREAMS" />
-              <Tab label="CHARTS" />
-            </Tabs>
+              <hr style={hudHR} />
+              <MeterWidget
+                log={log}
+                streamName="/vehicle/acceleration"
+                label="Acceleration"
+                min={-4}
+                max={4}
+              />
+              <hr style={hudHR} />
+              <MeterWidget
+                log={log}
+                streamName="/vehicle/velocity"
+                label="Speed"
+                getWarning={x => (x > 6 ? "FAST" : "")}
+                min={0}
+                max={20}
+              />
+            </div>
           </div>
-          {coordinates.length > 1 && (
-            <Map
-              onStyleLoad={map => {
-                mapHandler = map;
-              }}
-              id="map"
-              zoom={[15]}
-              onPitch={event =>
-                console.log(event.getPitch(), event.getBearing())
+          <div
+            style={{
+              padding: "24px 0",
+              position: "relative"
+            }}
+          >
+            <PlaybackControl
+              width="100%"
+              log={log}
+              formatTimestamp={x =>
+                new Date(x * TIMEFORMAT_SCALE).toUTCString()
               }
-              center={state.center}
-              style="mapbox://styles/mapbox/streets-v9"
-              containerStyle={{
-                height: "100%",
-                width: "75%"
-              }}
-              valueLabelDisplay="off"
-              onClick={event => console.log(event)}
-            >
-              <MapContext.Consumer>
-                {map => {
-                  console.log("hello");
-                  map.addLayer({
-                    id: "route",
-                    type: "line",
-                    source: {
-                      type: "geojson",
-                      data: {
-                        type: "Feature",
-                        properties: {},
-                        geometry: {
-                          type: "LineString",
-                          coordinates
-                        }
-                      }
-                    },
-                    layout: {
-                      "line-join": "round",
-                      "line-cap": "round"
-                    },
-                    paint: {
-                      "line-color": "#303030",
-                      "line-width": 8
-                    }
-                  });
-                }}
-              </MapContext.Consumer>
-            </Map>
-          )}
-        </div>
-        <div className={classes.row2}>
-          <div className={classes.dashboard}>Dashboard</div>
-          <div className={classes.scroll}>
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={state.jason}
-                  onChange={handleChange("jason")}
-                  value="jason"
-                />
-              }
-              label="Snap"
-            />
-            <CustomSlider
-              key={uuid()}
-              onChangeCommitted={(event, value) => {
-                if (previousID) mapHandler.removeLayer(previousID);
-                previousID = uuid();
-                mapHandler.flyTo(flyToOptions(coordinates[value]));
-                mapHandler.addLayer(FlyToMarker(coordinates[value]));
-              }}
-              valueLabelDisplay="off"
-              aria-label="pretto slider"
-              defaultValue={0}
-              min={0}
-              max={coordinates.length - 1}
-              step={1}
-              // marks={markers}
             />
           </div>
         </div>
-      </MainMapContext.Provider>
-    </div>
-  );
-};
-
-const flyToOptions = center => ({
-  center,
-  zoom: 20,
-  speed: 5,
-  curve: 1,
-  easing: t => t
-});
-
-const FlyToMarker = coordinates => ({
-  id: previousID,
-  type: "symbol",
-  source: {
-    type: "geojson",
-    data: {
-      type: "FeatureCollection",
-      features: [
-        {
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates
-          },
-          properties: {
-            title: "Current Position",
-            icon: "car",
-            "icon-size": 0.25
-          }
-        }
-      ]
-    }
-  },
-  layout: {
-    "icon-image": "{icon}-15",
-    "text-field": "{title}",
-    "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-    "text-offset": [0, 0.6],
-    "text-anchor": "top"
+      </div>
+    );
   }
-});
-
-const iOSBoxShadow =
-  "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)";
-
-const CustomSlider = withStyles(theme => ({
-  root: {
-    color: "#3880ff",
-    height: 2,
-    padding: "15px 0"
-  },
-  thumb: {
-    height: 28,
-    width: 28,
-    backgroundColor: "#fff",
-    boxShadow: iOSBoxShadow,
-    marginTop: -14,
-    marginLeft: -14,
-    "&:focus,&:hover,&$active": {
-      boxShadow:
-        "0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)",
-      "@media (hover: none)": {
-        boxShadow: iOSBoxShadow
-      }
-    }
-  },
-  active: {},
-  valueLabel: {
-    left: "calc(-50% + 11px)",
-    top: -22,
-    "& *": {
-      background: "transparent",
-      color: "#000"
-    }
-  },
-  track: {
-    height: 2
-  },
-  rail: {
-    height: 2,
-    opacity: 0.5,
-    backgroundColor: "#bfbfbf"
-  },
-  mark: {
-    backgroundColor: "#bfbfbf",
-    height: 8,
-    width: 1,
-    marginTop: -3
-  },
-  markActive: {
-    opacity: 1,
-    backgroundColor: "currentColor"
-  }
-}))(Slider);
+}
--- react/src/themes/Main.js
+++ react/src/themes/Main.js
@@ -5,17 +5,31 @@ const theme = createMuiTheme({
     fontFamily: "'Raleway','Barlow','Roboto'",
     useNextVariants: true,
   },
+  // palette: {
+  //   primary: {
+  //     light: '#be4962',
+  //     main: '#891438',
+  //     dark: '#560013',
+  //     contrastText: '#E0C1CB',
+  //   },
+  //   secondary: {
+  //     light: '#ffd776',
+  //     main: '#efa647',
+  //     dark: '#b87714',
+  //     contrastText: '#000000',
+  //   }
+  // },
   palette: {
     primary: {
-      light: '#be4962',
-      main: '#891438',
-      dark: '#560013',
+      light: '#344673',
+      main: '#001f47',
+      dark: '#000020',
       contrastText: '#E0C1CB',
     },
     secondary: {
-      light: '#ffd776',
-      main: '#efa647',
-      dark: '#b87714',
+      light: '#9ce9e1',
+      main: '#6bb7af',
+      dark: '#3a8780',
       contrastText: '#000000',
     }
   },

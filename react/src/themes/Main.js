bf50ec83800441d484fd8a8b06c3fbf2e6eff49b
Brendon Dulam <brendon.dulam@veoneer.com>
2019-10-09 16:55:50 +0900

    reverted the design

--- react/src/themes/Main.js
+++ react/src/themes/Main.js
@@ -5,34 +5,34 @@ const theme = createMuiTheme({
     fontFamily: "'Raleway','Barlow','Roboto'",
     useNextVariants: true,
   },
-  // palette: {
-  //   primary: {
-  //     light: '#be4962',
-  //     main: '#891438',
-  //     dark: '#560013',
-  //     contrastText: '#E0C1CB',
-  //   },
-  //   secondary: {
-  //     light: '#ffd776',
-  //     main: '#efa647',
-  //     dark: '#b87714',
-  //     contrastText: '#000000',
-  //   }
-  // },
   palette: {
     primary: {
-      light: '#344673',
-      main: '#001f47',
-      dark: '#000020',
+      light: '#be4962',
+      main: '#891438',
+      dark: '#560013',
       contrastText: '#E0C1CB',
     },
     secondary: {
-      light: '#9ce9e1',
-      main: '#6bb7af',
-      dark: '#3a8780',
+      light: '#ffd776',
+      main: '#efa647',
+      dark: '#b87714',
       contrastText: '#000000',
     }
   },
+  // palette: {
+  //   primary: {
+  //     light: '#344673',
+  //     main: '#001f47',
+  //     dark: '#000020',
+  //     contrastText: '#E0C1CB',
+  //   },
+  //   secondary: {
+  //     light: '#9ce9e1',
+  //     main: '#6bb7af',
+  //     dark: '#3a8780',
+  //     contrastText: '#000000',
+  //   }
+  // },
 });
 
 export default theme;
